06aeffc6490a2d8eb2746e26ad594e2dbeee3165
Hafsah <h.jilanee@rewaatech.com>
2024-11-14 16:02:04 +0500

    jwt

--- src/iam/authentication/authentication.controller.ts
+++ src/iam/authentication/authentication.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto/sign-up.dto';
+import { Response } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -13,7 +21,19 @@ export class AuthenticationController {
 
   @HttpCode(HttpStatus.OK)
   @Post('sign-in')
-  signIn(@Body() signInDto: SignUpDto) {
+  async signIn(
+    //@Res({ passthrough: true }) response: Response,
+    @Body() signInDto: SignUpDto,
+  ) {
+    //cookie approach, accesstoken will show in cookie tab in insomnia. this is a safer approach
+
+    // const accessToken = await this.authService.signIn(signInDto);
+    // response.cookie('accessToken', accessToken, {
+    //   secure: true,
+    //   httpOnly: true,
+    //   sameSite: true,
+    // });
+
     return this.authService.signIn(signInDto);
   }
 }
